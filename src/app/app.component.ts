ecdc20de0d21f3add2b9095da7ed3083c445d78d
nigelsmk <kahler.maynard@gmail.com>
2017-02-24 15:58:42 -0500

    analyer switching complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Speaker} from "./models/speaker.model";
+import {UserMediaService} from "./services/user-media.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+
+  public speakers: Speaker[] = new Array<Speaker>();
+  private analyser: AnalyserNode;
+  private audioCtx: AudioContext;
+
+  constructor(private userMedia: UserMediaService) {
+    // mock speakers
+    this.speakers.push({name: 'Nigel'});
+    this.speakers.push({name: 'Anton'});
+  }
+
+  setActive(speaker: Speaker) {
+    this.speakers.forEach((speaker) => {
+      speaker.analyser = null;
+    });
+    speaker.analyser = this.analyser;
+  }
+
+  ngOnInit() {
+    this.userMedia.streams.subscribe((stream: MediaStream) => {
+      this.audioCtx = new AudioContext();
+      let source = this.audioCtx.createMediaStreamSource(stream);
+      this.analyser = this.audioCtx.createAnalyser();
+      source.connect(this.analyser); // connect analyser to mic input
+
+      this.speakers[0].analyser = this.analyser; // attach analyser to first speaker
+    });
+
+    this.userMedia.fetchStream({ audio: true }); // request user permission to access mic;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SpeakerCardComponent } from './components/speaker-card/speaker-card.component';
+import { EqualizerComponent } from './components/equalizer/equalizer.component';
+import {UserMediaService} from "./services/user-media.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpeakerCardComponent,
+    EqualizerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [UserMediaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
